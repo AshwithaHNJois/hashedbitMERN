96273b9f616564d52b300be42911d5a6f7a0d0f9
Ashwitha H N Jois <130340869+AshwithaHNJois@users.noreply.github.com>
2023-11-19 13:29:20 +0530

    Add files via upload

---
+++ Assmnt4.js
@@ -0,0 +1,6 @@
+DOM
+//1. https://jsfiddle.net/AshwithaHNJois/n68jbet3/7/
+//2. https://jsfiddle.net/AshwithaHNJois/nLq5u8dg/15/
+//3. https://jsfiddle.net/AshwithaHNJois/md0pe9ro/34/
+//4. https://jsfiddle.net/AshwithaHNJois/oyrn4xtv/53/
+
