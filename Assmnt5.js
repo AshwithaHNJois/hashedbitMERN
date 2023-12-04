8fac0e3478c4377835ac3c804182560e0d25c65b
Ashwitha H N Jois <130340869+AshwithaHNJois@users.noreply.github.com>
2023-12-04 16:43:19 +0530

    Update Assmnt5.js

--- Assmnt5.js
+++ Assmnt5.js
@@ -1,3 +1,5 @@
-console.log(HOT || COLD);
+DOM %
 // https://jsfiddle.net/AshwithaHNJois/qn3p67t9/13/
+// https://jsfiddle.net/AshwithaHNJois/ob04gdnj/5/
+// https://jsfiddle.net/AshwithaHNJois/nd35vzo6/3/  
 
